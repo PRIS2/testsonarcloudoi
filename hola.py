b477b20d5d641daa8e2b5a6b34af5120f729c4b9
Isa del Águila <37574770+imaguila@users.noreply.github.com>
2022-03-23 11:51:18 +0100

    Update hola.py

--- hola.py
+++ hola.py
@@ -7,3 +7,10 @@ print("Hola Mundo");
 texto=input('Introduzca un numero: '))
 
 num = int(input('Introduzca un numero: '))
+
+
+###Ejemplo foreach, imprime los numeros de la lista
+
+for i in [1, 5, 7]:
+print(i, end=" ") #imprimir numero, sin salto linea
+print() #lineavacia
